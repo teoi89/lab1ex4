e67e683447ceedb0731e464401b3f36b3b7b1cf3
teoi89 <teofil.lucaci@yahoo.ro>
2022-11-28 16:58:45 +0200

    Add project files.

---
+++ lab1ex4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace lab1ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care va afisa semnul unui numar citit de la tastatura
+                • Daca numarul este pozitiv, va afisa “+”
+                • Daca numarul este negativ, va afisa “-”
+                • Daca numarul este 0, va afisa “0” */
+
+            Console.WriteLine("Introduceti un numar:");
+            
+            int number = int.Parse(Console.ReadLine());
+
+            if (number < 0)
+            {
+                Console.WriteLine("-");
+            }
+            else if(number > 0)
+            {
+                Console.WriteLine("+");
+            }
+            else if(number == 0)
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
